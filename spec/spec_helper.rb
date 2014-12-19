73070b411aa963f12a94dfa662590989406da0b8
fiestacasey <casey@sobrilsky.com>
2014-12-19 12:19:42 -0600

    whoops

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,8 @@ require 'card_connect_gateway'
 CardConnectGateway.configure do |config|
   config.test_mode = true
   config.merchant_id = ENV["CARD_CONNECT_MERCHANT_ID"]
-  config.user_id = 'CARD_CONNECT_USER_ID'
-  config.password = 'CARD_CONNECT_PASSWORD'
+  config.user_id = ENV['CARD_CONNECT_USER_ID']
+  config.password = ENV['CARD_CONNECT_PASSWORD']
 end
 
 RSpec.configure do |config|
