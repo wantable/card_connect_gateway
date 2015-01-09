aa1bc81233d92cce452f0bfce467f7e103a67a0e
fiestacasey <casey@sobrilsky.com>
2015-01-09 14:15:07 -0600

    that code quality yo @ksolkowski

--- lib/card_connect_gateway/base_response.rb
+++ lib/card_connect_gateway/base_response.rb
@@ -9,8 +9,21 @@ module CardConnectGateway
     FirstDataNorth = 'FNOR'
 
     def validate
-      raise NotImplementedException
+      self.errors = {}
+
+      if respstat != APPROVED
+        if resptext and !resptext.empty? and respproc and !respproc.empty?
+          errors[respproc.to_sym] = resptext 
+        else
+          errors["Card Connect Gateway"] = I18n.t(:unknown_error)
+        end
+      end
+
+      @validated = true
+
+      errors.empty?
     end
+
 
     def valid?
       @validated ? self.errors.empty? : validate
