512f413f508d63d1912437de78c78a9f9fb8c44e
fiestacasey <casey@sobrilsky.com>
2015-01-09 14:56:04 -0600

    refactoring...?

--- lib/card_connect_gateway/base_request.rb
+++ lib/card_connect_gateway/base_request.rb
@@ -10,25 +10,7 @@ module CardConnectGateway
     def validate
       self.errors ||= {}
       self.class.attributes.each do |key, validations|
-        value = get_value(key)
-        if value.nil? or value.empty?
-          req = validations[:required]
-          if req == true or (req.class == Proc and req.call(self) == true)
-            self.errors[key] = I18n.t(:is_required)
-          end
-        else 
-          if maxLength = validations[:maxLength]
-            self.errors[key] = I18n.t(:cannot_be_longer_than, maxLength: maxLength) if value.length > maxLength
-          end
-
-          if options = validations[:options]
-            self.errors[key] = I18n.t(:must_be_one_of, options: options) if !options.include?(value)
-          end
-
-          if format = validations[:format]
-            self.errors[key] = I18n.t(:doesnt_match_the_format) if !(format =~ value)
-          end
-        end
+        validate_attribute(key, validations)
       end
 
       @validated = true
@@ -67,27 +49,56 @@ module CardConnectGateway
       super(mapped_options)
     end
 
-    def to_hash
-      self.class.attributes.keys.inject({}) do |hash, key|
-        value = get_value(key)
-        hash[key] = value if value
-        hash
-      end
-    end
-
     def send
-      url = "https://#{CardConnectGateway.configuration.user_id}:#{CardConnectGateway.configuration.password}@"
-      url += "#{CardConnectGateway.configuration.url.gsub("http://","").gsub("https://","")}/#{self.class.resource_name}"
-
+      url = build_url
       puts "sending PUT request to #{url}\r\nwith content\r\n#{to_hash.to_json}" if CardConnectGateway.configuration.debug
       
       begin
         response = RestClient.put(url, self.to_hash.to_json, content_type: :json, accept: :json)
         puts "response: #{response}" if CardConnectGateway.configuration.debug
+
         JSON.parse(response)
       rescue => e
         puts "error: #{e}"  if CardConnectGateway.configuration.debug
         e
+      end
+    end
+
+    private 
+
+    def validate_attribute(key, validations=[])
+      value = get_value(key)
+      if value.nil? or value.empty?
+        req = validations[:required]
+        if req == true or (req.class == Proc and req.call(self) == true)
+          self.errors[key] = I18n.t(:is_required)
+        end
+      else 
+        if maxLength = validations[:maxLength]
+          self.errors[key] = I18n.t(:cannot_be_longer_than, maxLength: maxLength) if value.length > maxLength
+        end
+
+        if options = validations[:options]
+          self.errors[key] = I18n.t(:must_be_one_of, options: options) if !options.include?(value)
+        end
+
+        if format = validations[:format]
+          self.errors[key] = I18n.t(:doesnt_match_the_format) if !(format =~ value)
+        end
+      end
+    end
+
+    def build_url
+      url = "https://#{CardConnectGateway.configuration.user_id}:#{CardConnectGateway.configuration.password}@"
+      url += "#{CardConnectGateway.configuration.url.gsub("http://","").gsub("https://","")}/#{self.class.resource_name}"
+      url
+    end
+
+    def to_hash
+      self.class.attributes.keys.inject({}) do |hash, key|
+        value = get_value(key)
+        hash[key] = value if value
+        hash
       end
     end
 
