1519bb6d4feb058dff0b864c14a09aa9817590bd
fiestacasey <casey@sobrilsky.com>
2015-01-22 11:58:40 -0600

    test for partial void

--- spec/lib/void_spec.rb
+++ spec/lib/void_spec.rb
@@ -25,4 +25,23 @@ describe "Void" do
     void.valid?
     expect(void.errors).to eq({}) 
   end
+
+  it 'partial void successfully' do
+    auth = CardConnectGateway.authorization({
+      account: VISA_APPROVAL_ACCOUNT,
+      expiry: '0921',
+      cvv2: CVV_MATCH,
+      postal: VISA_AVS_MATCH_ZIP,
+      amount: 20
+    })
+
+    void = CardConnectGateway.void({
+      retref: auth.retref,
+      amount: 5
+    })
+
+    void.valid?
+    expect(void.errors).to eq({}) 
+    expect(void.amount).to eq('15.00')
+  end
 end
